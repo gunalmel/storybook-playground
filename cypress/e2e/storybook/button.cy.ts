8dde5ac7848567b9789456d1915906e230fa28d9
Melih Gunal <gunalmel@med.umich.edu>
2022-09-30 23:21:14 -0400

    Initialize project using Create React App

---
+++ cypress/e2e/storybook/button.cy.ts
@@ -0,0 +1,21 @@
+
+
+/*const getIframeBody = () => {
+// get the iframe > document > body
+    return cy.get('iframe[id="storybook-preview-iframe"]')
+    // and retry until the body element is not empty
+.its('0.contentDocument.body').should('not.be.empty')
+    // wraps "body" DOM element
+    // https://on.cypress.io/wrap
+.then(cy.wrap);
+}
+*/
+describe("Button", () => {
+    before(() => {
+        cy.visit("/iframe.html?id=example-button--primary");
+    });
+    it("loads primary button with default text", () => {
+        cy.get('storybook-button storybook-button--medium storybook-button--primary').contains('button', 'Button');
+        // getIframeBody().get('#root').contains('button', 'Button');
+    });
+});
